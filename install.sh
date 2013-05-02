059e67e226ae7db9beff3d2554c428282390b6e5
Tyler Stroud <tyler@tylerstroud.com>
2013-05-02 12:50:31 -0700

    Added git-completion

--- install.sh
+++ install.sh
@@ -6,3 +6,8 @@ ln -s .dotfiles/vim/.vim ~/.vim
 
 # Git
 ln -s .dotfiles/git/.gitconfig ~/.gitconfig
+
+ln -s .dotfiles/git/git-completion.bash ~/.git-completion.bash
+if ! grep -q "source ~/.git-completion.bash" ~/.bash_profile; then
+    echo "source ~/.git-completion.bash" >> ~/.bash_profile;
+fi
